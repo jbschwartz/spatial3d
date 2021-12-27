8679c33dd8835c79732450b6636957ee35bc79e5
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-27 11:26:29 -0500

    Update AABB.
    Simplify AABB construction.
    Implement axis_extents function.
    Implement cached_properties.
    Raise an exception if splitting outside of the bounding box.

--- test/test_aabb.py
+++ test/test_aabb.py
@@ -10,14 +10,24 @@ class TestAABB(unittest.TestCase):
         self.v3 = Vector3(0, 1, 2)
         self.v4 = Vector3(4, 4, 4)
 
-        self.aabb = AABB.from_points([self.v1, self.v2, self.v3])
+        self.aabb = AABB([self.v1, self.v2, self.v3])
+
+    def test_center_returns_the_center_of_the_bounding_box(self):
+        expected = (self.v1 - self.v2) / 2 + self.v2
+
+        self.assertAlmostEqual(self.aabb.center, expected)
+
+    def test_center_on_empty_aabb_is_origin(self):
+        empty = AABB()
+
+        self.assertAlmostEquals(empty.center, Vector3(0, 0, 0))
 
     def test_expand(self):
         self.assertAlmostEqual(self.v2, self.aabb.min)
         self.assertAlmostEqual(self.v1, self.aabb.max)
 
     def test_expand_aabb(self):
-        other_aabb = AABB(self.v3, self.v4)
+        other_aabb = AABB([self.v3, self.v4])
 
         self.aabb.expand(other_aabb)
 
@@ -25,9 +35,6 @@ class TestAABB(unittest.TestCase):
         self.assertAlmostEqual(self.v4, self.aabb.max)
 
     def test_split(self):
-        # TODO: Make this into a nice decorator to sprinkle throughout test cases
-        self.longMessage = False
-
         value = -0.5
 
         for index in range(0, 3):
@@ -54,26 +61,7 @@ class TestAABB(unittest.TestCase):
 
         self.assertAlmostEqual(self.aabb.size, expected)
 
-    def test_center(self):
-        expected = (self.v1 - self.v2) / 2 + self.v2
-
-        self.assertAlmostEqual(self.aabb.center, expected)
-
-    def test_center_on_empty_aabb_is_origin(self):
-        empty = AABB()
-
-        self.assertAlmostEquals(empty.center, Vector3(0, 0, 0))
-
-    def test_is_empty_is_true_for_default_constructed_aabb(self):
-        empty = AABB()
-
-        self.assertTrue(empty.is_empty)
-
-    def test_is_empty_is_false_for_expanded_aabb(self):
-        self.assertFalse(self.aabb.is_empty)
-
     def test_contains(self):
-        # Test contains with points
         self.assertTrue(self.aabb.contains(self.v3))
         self.assertFalse(self.aabb.contains(self.v4))
 
