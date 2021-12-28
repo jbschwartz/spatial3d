20555e925e0626adc44f313f1c99cbcf37bb88f4
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-28 12:56:26 -0500

    Reorder Vector3 methods.
    Include a default value for round magic function.
    Remove Vector3.transform function.

--- spatial/vector3.py
+++ spatial/vector3.py
@@ -28,57 +28,18 @@ class Vector3(Swizzler):
         return cls(0, 0, 1)
 
     def __abs__(self) -> "Vector3":
+        """Return a vector with the component-wise absolute values of this vector."""
         return Vector3(abs(self.x), abs(self.y), abs(self.z))
 
-    def __round__(self, places: int) -> "Vector3":
-        return Vector3(round(self.x, places), round(self.y, places), round(self.z, places))
-
     def __add__(self, other: "Vector3") -> "Vector3":
+        """Return a vector with the component-wise sum of this vector and the other."""
         if isinstance(other, Vector3):
             return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
 
         return NotImplemented
 
-    def __sub__(self, other: "Vector3") -> "Vector3":
-        if isinstance(other, Vector3):
-            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
-
-        return NotImplemented
-
-    def __neg__(self) -> "Vector3":
-        return Vector3(-self.x, -self.y, -self.z)
-
-    @overload
-    def __mul__(self, other: "Vector3") -> float:
-        ...
-
-    @overload
-    def __mul__(self, other: Union[float, int]) -> "Vector3":
-        ...
-
-    def __mul__(self, other: Union["Vector3", float, int]) -> Union["Vector3", float]:
-        if isinstance(other, (float, int)):
-            return Vector3(other * self.x, other * self.y, other * self.z)
-
-        if isinstance(other, Vector3):
-            # Dot product
-            return self.x * other.x + self.y * other.y + self.z * other.z
-
-        return NotImplemented
-
-    __rmul__ = __mul__
-
-    def __mod__(self, other: "Vector3") -> "Vector3":
-        """Vector3 cross product."""
-        return cross(self, other)
-
-    def __truediv__(self, other: Union[float, int]) -> "Vector3":
-        if isinstance(other, (float, int)):
-            return Vector3(self.x / other, self.y / other, self.z / other)
-
-        return NotImplemented
-
     def __eq__(self, other: object) -> bool:
+        """Return True if this vector is equal to the other."""
         if isinstance(other, Vector3):
             return self.x == other.x and self.y == other.y and self.z == other.z
 
@@ -91,24 +52,77 @@ class Vector3(Swizzler):
 
         return NotImplemented
 
-    def __str__(self) -> str:
-        return f"({self.x}, {self.y}, {self.z})"
-
     def __getitem__(self, index: int) -> float:
+        """Return the value of the component at the provided index."""
         components = ["x", "y", "z"]
         return getattr(self, components[index])
 
+    def __len__(self) -> int:
+        """Return the number of (coordinate) dimensions in this vector."""
+        return 3
+
+    def __mod__(self, other: "Vector3") -> "Vector3":
+        """Return the cross product of this vector with the other."""
+        return cross(self, other)
+
+    @overload
+    def __mul__(self, other: "Vector3") -> float:  # noqa: D105
+        ...
+
+    @overload
+    def __mul__(self, other: Union[float, int]) -> "Vector3":  # noqa: D105
+        ...
+
+    def __mul__(self, other: Union["Vector3", float, int]) -> Union["Vector3", float]:
+        """Return the dot product of this vector with the other.
+
+        If `other` is a scalar, return a vector with the component-wise multiple of this vector.
+        """
+        if isinstance(other, (float, int)):
+            return Vector3(other * self.x, other * self.y, other * self.z)
+
+        if isinstance(other, Vector3):
+            # Dot product
+            return self.x * other.x + self.y * other.y + self.z * other.z
+
+        return NotImplemented
+
+    def __neg__(self) -> "Vector3":
+        """Return a vector with the component-wise negation of this vector."""
+        return Vector3(-self.x, -self.y, -self.z)
+
+    __rmul__ = __mul__
+
+    def __round__(self, places: int = 0) -> "Vector3":
+        """Return a vector with the component-wise rounded values of this vector."""
+        return Vector3(round(self.x, places), round(self.y, places), round(self.z, places))
+
     def __setitem__(self, index: int, value: float) -> None:
+        """Set the value of the component at the provided index."""
         components = ["x", "y", "z"]
         setattr(self, components[index], value)
 
-    def __len__(self) -> int:
-        """Return the number of (coordinate) dimensions."""
-        return 3
+    def __str__(self) -> str:
+        """Return the string representation of this vector."""
+        return f"({self.x}, {self.y}, {self.z})"
 
-    def transform(self, transform, as_type: str = "point") -> "Vector3":
-        """Return the a new vector which has transformed."""
-        return transform(self, as_type=as_type)
+    def __sub__(self, other: "Vector3") -> "Vector3":
+        """Return a vector with the component-wise difference of this vector and the other."""
+        if isinstance(other, Vector3):
+            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+        return NotImplemented
+
+    def __truediv__(self, other: Union[float, int]) -> "Vector3":
+        """Return a vector with the component-wise scalar quotient of this vector."""
+        if isinstance(other, (float, int)):
+            return Vector3(self.x / other, self.y / other, self.z / other)
+
+        return NotImplemented
+
+    def is_unit(self, tolerance: float = 0.00001) -> bool:
+        """Return True if the vector has unit length (within a given tolerance)."""
+        return math.isclose(self.length_sq(), 1.0, abs_tol=tolerance)
 
     def length(self) -> float:
         """Return the length of the vector."""
@@ -117,10 +131,6 @@ class Vector3(Swizzler):
     def length_sq(self) -> float:
         """Return the squared length of the vector."""
         return self.x ** 2 + self.y ** 2 + self.z ** 2
-
-    def is_unit(self, tolerance: float = 0.00001) -> bool:
-        """Return True if the vector has unit length (within a given tolerance)."""
-        return math.isclose(self.length_sq(), 1.0, abs_tol=tolerance)
 
     def normalize(self) -> "Vector3":
         """Normalize this vector to unit length.
@@ -134,12 +144,24 @@ class Vector3(Swizzler):
         return self
 
 
+def almost_equal(v1: Vector3, v2: Vector3, tol=0.00001) -> bool:
+    """Return True if two vectors are equal within a given tolerance."""
+    difference = v1 - v2
+
+    return math.isclose(difference.length_sq(), 0.0, abs_tol=tol)
+
+
 def angle_between(v1: Vector3, v2: Vector3) -> float:
     """Return the angle between two vectors in radians."""
     dot = v1 * v2
     lengths = v1.length() * v2.length()
 
     return math.acos(dot / lengths)
+
+
+def cross(v1: Vector3, v2: Vector3) -> Vector3:
+    """Return the cross product of two vectors."""
+    return Vector3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x)
 
 
 def normalize(v: Vector3) -> Vector3:
@@ -149,15 +171,3 @@ def normalize(v: Vector3) -> Vector3:
     """
     length = v.length()
     return Vector3(v.x / length, v.y / length, v.z / length)
-
-
-def cross(v1: Vector3, v2: Vector3) -> Vector3:
-    """Return the cross product of two vectors."""
-    return Vector3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x)
-
-
-def almost_equal(v1: Vector3, v2: Vector3, tol=0.00001) -> bool:
-    """Return True if two vectors are equal within a given tolerance."""
-    difference = v1 - v2
-
-    return math.isclose(difference.length_sq(), 0.0, abs_tol=tol)
