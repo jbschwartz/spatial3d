0c202bba31e99734fd9fbe3865ba01436e8ea69c
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-29 14:33:33 -0500

    Clean up linting errors in Dual.

--- spatial/dual.py
+++ spatial/dual.py
@@ -1,74 +1,81 @@
-from . import quaternion
+from typing import Generic, TypeVar, Union
 
-Quaternion = quaternion.Quaternion
+from .quaternion import Quaternion, conjugate as quaternion_conjugate
+
+T = TypeVar("T", float, int, Quaternion)
 
 
-class Dual:
-    """
-    Class for representing dual numbers of the form r + dε
-    """
+class Dual(Generic[T]):
+    """Class for representing dual (numbers) of the form r + dε."""
 
-    def __init__(self, r, d):
-        self.r = r
-        self.d = d
+    def __init__(self, r: T, d: T) -> None:
+        self.r: T = r
+        self.d: T = d
 
-    def __abs__(self):
+    def __abs__(self) -> "Dual":
         return Dual(abs(self.r), abs(self.d))
 
-    def __round__(self, n):
-        return Dual(round(self.r, n), round(self.d, n))
+    def __round__(self, places: int) -> "Dual":
+        return Dual(round(self.r, places), round(self.d, places))
 
-    def __add__(self, other):
+    def __add__(self, other: "Dual") -> "Dual":
         return Dual(self.r + other.r, self.d + other.d)
 
-    def __sub__(self, other):
+    def __sub__(self, other: "Dual") -> "Dual":
         return Dual(self.r - other.r, self.d - other.d)
 
-    def __mul__(self, other):
-        """
-        Dual quaternion multiplication
+    def __mul__(self, other: Union["Dual", float, int]) -> "Dual":
+        """Dual multiplication.
 
-        If passed a number, scalar multiplication is performed.
-        If passed another dual quaternion, dual quaternion multiplication is performed.
+        If passed a scalar, scalar multiplication is performed.
+        If passed a dual, dual multiplication is performed.
         """
-        if isinstance(other, (int, float)):
+        if isinstance(other, (float, int)):
             return Dual(other * self.r, other * self.d)
-        elif isinstance(other, Dual):
+
+        if isinstance(other, Dual):
             return Dual(self.r * other.r, self.r * other.d + self.d * other.r)
+
+        return NotImplemented
 
     __rmul__ = __mul__
 
-    def __truediv__(self, other):
-        if isinstance(other, (int, float)):
+    def __truediv__(self, other: Union[float, int]) -> "Dual":
+        if isinstance(other, (float, int)):
             reciprocal = 1 / other
             return reciprocal * self
 
-    def __eq__(self, other):
+        return NotImplemented
+
+    def __eq__(self, other) -> bool:
         if isinstance(other, Dual):
             return self.r == other.r and self.d == other.d
-        elif isinstance(other, int):
+
+        if isinstance(other, int):
             if other == 0:
-                # Used to check Dual == 0 (i.e. check if Dual is the zero vector)
+                # Used to check Dual == 0 (i.e. check if Dual is the zero Dual)
                 # This is useful for unittest.assertAlmostEqual
                 return self.r == 0 and self.d == 0
 
-    def __str__(self):
+        return NotImplemented
+
+    def __str__(self) -> str:
         return f"{self.r} + {self.d}" + "\u03B5"
 
-    def conjugate(self):
-        """
-        Conjugates the dual instance
-        """
+    def conjugate(self) -> None:
+        """Conjugates the dual instance."""
         if isinstance(self.r, Quaternion) and isinstance(self.d, Quaternion):
             self.r.conjugate()
-            self.d = -quaternion.conjugate(self.d)
+            self.d = -quaternion_conjugate(self.d)
         elif isinstance(self.r, (int, float)) and isinstance(self.d, (int, float)):
             self.d = -self.d
+        else:
+            raise NotImplementedError
 
-    def norm(self):
-        pass
 
+def conjugate(dual: Dual) -> Dual:
+    """Return the conjugate of the provided Dual quaternion."""
+    if isinstance(dual.r, Quaternion) and isinstance(dual.d, Quaternion):
+        return Dual(quaternion_conjugate(dual.r), -quaternion_conjugate(dual.d))
 
-def conjugate(obj):
-    if isinstance(obj.r, Quaternion) and isinstance(obj.d, Quaternion):
-        return Dual(quaternion.conjugate(obj.r), -quaternion.conjugate(obj.d))
+    raise NotImplementedError
