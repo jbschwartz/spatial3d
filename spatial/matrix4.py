a5e617ff49850b1eb1043489f49247fd9abe35e4
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-29 15:37:56 -0500

    Clean up linting errors in Matrix4.

--- spatial/matrix4.py
+++ spatial/matrix4.py
@@ -3,64 +3,50 @@ from typing import Iterable
 from .dual import Dual
 from .quaternion import Quaternion, conjugate
 from .transform import Transform
+from .vector3 import Vector3
 
 
 class Matrix4:
-    def __init__(self, elements: Iterable[float] = None) -> None:
-        """4x4 Matrix in _column-major_ order.
+    """A column-major 4 x 4 Matrix.
 
-        That is: contiguous elements in the list form columns (e.g. self.elements[0:4] is the first column of the matrix)
-        """
+    Elements in groups of four form columns (e.g. self.elements[0:4] is the first column).
+    """
+
+    def __init__(self, elements: Iterable[float] = None) -> None:
         if elements:
             if len(elements) != 16:
-                raise TypeError("Matrix4 requires 16 floating point elements")
+                raise TypeError(
+                    f"Matrix4 requires 16 floating point elements. Received {len(elements)}"
+                )
 
             self.elements = elements
         else:
-            self.elements = [
-                1.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                1.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                1.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                1.0,
-            ]
+            self.elements = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
 
     @classmethod
     def from_transform(cls, transform: Transform) -> "Matrix4":
-        """Construct a Matrix4 from a Transform."""
+        """Construct a matrix from a transform."""
         return cls.from_dual(transform.dual)
 
     @classmethod
-    def from_dual(cls, dual: Dual) -> "Matrix4":
-        """Construct a Matrix4 from a Dual Quaternion."""
+    def from_dual(cls, dual: Dual[Quaternion]) -> "Matrix4":
+        """Construct a matrix from a dual quaternion."""
         elements = []
 
         r_star = conjugate(dual.r)
-        for basis in [(0, 1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1)]:
-            transformed_basis = dual.r * Quaternion(*basis) * r_star
+        for basis in [Vector3.X(), Vector3.Y(), Vector3.Z()]:
+            transformed_basis = dual.r * Quaternion(0, *basis) * r_star
             elements.extend([*transformed_basis.xyz, 0])
 
         translation = 2 * dual.d * r_star
         elements.extend([*translation.xyz, 1])
 
-        assert len(elements) == 16
-
         return cls(elements)
 
     def __str__(self) -> str:
+        """Return the string representation of this matrix."""
         # Get the width of "widest" floating point number
-        longest = max(map(len, map("{:.4f}".format, self.elements)))
+        longest = max(map(len, map(lambda e: f"{e:.4f}", self.elements)))
         # Pad the left of each element to the widest number found
         padded = list(map(lambda elem: f"{elem:>{longest}.4f}", self.elements))
 
