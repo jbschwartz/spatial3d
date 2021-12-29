31a9f8b136e4bc94ccd602340ad0c33017e4bc6d
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-29 14:07:31 -0500

    Document and unit test Swizzler base class.

--- spatial/swizzler.py
+++ spatial/swizzler.py
@@ -1,6 +1,28 @@
+# This class is meant to be inherited from so the lack of public methods is normal.
+# pylint: disable=too-few-public-methods
+
+from typing import Any
+
+
 class Swizzler:
-    def __getattr__(self, name):
-        def allow(char):
+    """A base class for composing component parameters at runtime.
+
+    For example, a Vector3 v with three components (v.x, v.y, v.z) can be accessed arbitrarily:
+        v.xyx returns [v.x, v.y, v.x]
+
+    See Vector3 and Quaternion.
+    """
+
+    def __getattr__(self, name: str) -> list[Any]:
+        """Return a list of values of the composed parameters.
+
+        All parameters are considered to be a single letter long.
+
+        This function raises an AttributeError if the parameter does not exist.
+        """
+
+        def allow(char: str) -> Any:
+            """Return the value of the provided parameter."""
             if char not in self.__slots__:
                 raise AttributeError
 
