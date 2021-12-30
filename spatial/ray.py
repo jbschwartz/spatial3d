f5d937e33953e16c197df12f749917dcef7ce526
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-30 11:31:08 -0500

    Clean up linting errors in Ray.

--- spatial/ray.py
+++ spatial/ray.py
@@ -1,22 +1,27 @@
 from typing import Iterable
 
 from .intersection import Intersection
+from .transform import Transform
+from .vector3 import Vector3
 
 
 class Ray:
-    def __init__(self, origin, direction):
+    """A ray given an origin and a direction vector."""
+
+    def __init__(self, origin: Vector3, direction: Vector3) -> None:
         self.origin = origin
         # TODO: What about a zero-length direction vector?
         self.direction = direction.normalize()
 
-    def transform(self, transform: "Transform") -> "Ray":
-        """Return a new transformed ray."""
-        new_origin = self.origin.transform(transform, as_type="point")
-        new_direction = self.direction.transform(transform, as_type="vector")
+    def transform(self, transform: Transform) -> "Ray":
+        """Return a transformed ray given the provided transform."""
+        new_origin = transform(self.origin, as_type="point")
+        new_direction = transform(self.direction, as_type="vector")
 
         return Ray(new_origin, new_direction)
 
-    def closest_intersection(self, collection: Iterable) -> Intersection:
+    def closest_intersection(self, collection: Iterable[object]) -> Intersection:
+        """Return the closest intersection of the ray with the collection of objects."""
         closest = Intersection.Miss()
 
         for item in collection:
@@ -29,9 +34,10 @@ class Ray:
 
         return closest
 
-    def evaluate(self, t):
+    def evaluate(self, t: float) -> Vector3:
         """Return the location along ray given parameter t."""
         return self.origin + t * self.direction
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return the string representation of this ray."""
         return f"{self.origin} + t * {self.direction}"
