dde0569095228c61b70daefea94d5a37d5c53b34
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-30 12:02:30 -0500

    Overhaul Ray test cases.

--- test/test_ray.py
+++ test/test_ray.py
@@ -1,9 +1,6 @@
-import math
-import unittest
+import math, unittest
 
-from spatial import Ray, vector3
-
-Vector3 = vector3.Vector3
+from spatial import Facet, Ray, Transform, Vector3, vector3
 
 
 class TestRay(unittest.TestCase):
@@ -11,15 +8,52 @@ class TestRay(unittest.TestCase):
         self.direction = Vector3(-1, 4, 5)
         self.ray = Ray(Vector3(1, 2, 3), self.direction)
 
-    def test_init(self):
-        result = self.ray.direction
-        expected = vector3.normalize(self.direction)
+    def test__init__normalized_the_direction(self):
+        self.assertAlmostEqual(self.ray.direction, vector3.normalize(self.direction))
 
-        self.assertAlmostEqual(result, expected)
+    def test__init__raises_if_direction_is_zero_vector(self):
+        with self.assertRaises(ValueError):
+            Ray(Vector3(), Vector3())
 
-    def test_evaluate(self):
-        # TODO: Needs more testing
-        result = self.ray.evaluate(0)
-        expected = self.ray.origin
+    def test__str__contains_the_components_of_the_ray(self) -> None:
+        self.assertTrue(str(self.ray.origin) in str(self.ray))
 
-        self.assertAlmostEqual(result, expected)
+        normalized = vector3.normalize(self.ray.direction)
+        self.assertTrue(str(normalized) in str(self.ray))
+
+    def test_closest_intersection_returns_the_closest_intersection(self) -> None:
+        facets = [
+            Vector3(),  # Ignored by the algorithm.
+            Facet([Vector3(), Vector3.X(), Vector3.Y()]),
+            Facet([Vector3(0, 0, 1), Vector3(1, 0, 1), Vector3(0, 1, 1)]),
+        ]
+
+        r = Ray(Vector3(0, 0, 2), Vector3.Z())
+        actual = r.closest_intersection(facets)
+
+        self.assertIsNone(actual.t)
+        self.assertIsNone(actual.obj)
+
+        r = Ray(Vector3(0, 0, 2), -Vector3.Z())
+        actual = r.closest_intersection(facets)
+
+        self.assertEqual(actual.t, 1)
+        self.assertEqual(actual.obj, facets[2])
+
+    def test_evaluate_returns_the_location_along_the_ray(self) -> None:
+        self.assertEqual(self.ray.evaluate(0), self.ray.origin)
+        self.assertEqual(
+            self.ray.evaluate(2), self.ray.origin + 2 * vector3.normalize(self.ray.direction)
+        )
+
+    def test_tranform_transforms_the_rays_origin_and_direction(self) -> None:
+        r = Ray(Vector3(1, 1, 0), Vector3(1, 1, 0))
+        t = Transform.from_axis_angle_translation(
+            axis=Vector3.Z(), angle=math.radians(-45), translation=Vector3(-math.sqrt(2), 0, 0)
+        )
+
+        expected = Ray(Vector3(), Vector3.X())
+        actual = r.transform(t)
+
+        self.assertAlmostEqual(actual.origin, expected.origin)
+        self.assertAlmostEqual(actual.direction, expected.direction)
