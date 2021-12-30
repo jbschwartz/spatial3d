db3d2785f68424a2fc1eb5ac7a4603aa4d6a193d
James Schwartz <jbschwartz@users.noreply.github.com>
2021-12-30 14:25:18 -0500

    Reorder KDTree methods.
    Change is_leaf to property.

--- spatial/kdtree.py
+++ spatial/kdtree.py
@@ -18,47 +18,10 @@ class KDTreeNode:
         self.facets = facets
         self.children: list[KDTreeNode] = []
 
+    @property
     def is_leaf(self) -> bool:
         """Return true if the this node is a leaf node (i.e., it has no children)."""
         return len(self.children) == 0
-
-    def can_branch(self, depth: int) -> bool:
-        """Return true if this node can be split into two child nodes."""
-        # TODO: Maybe use a more sophisticated cost function to evaluate whether we should branch.
-        return len(self.facets) > 0 and depth < DEPTH_BOUND
-
-    def splitting_plane(self, depth: int) -> Tuple[CoordinateAxes, float]:
-        """Return a tuple with the splitting plane axis and value for the given depth."""
-        # TODO: Maybe use a more sophisticated splitting plane generation like SAH.
-        plane_axis = CoordinateAxes(depth % 3)
-        plane_value = self.aabb.center[plane_axis]
-
-        return plane_axis, plane_value
-
-    def split_facets(
-        self, plane_axis: CoordinateAxes, plane_value: float
-    ) -> Tuple[list[Facet], list[Facet]]:
-        """Split the node's list of facets into left and right lists based on splitting plane."""
-        left, right = [], []
-
-        for facet in self.facets:
-            # Check minimum bound on left and maximum bound on right.
-            # This covers both scenarios: facets belonging to one side only or both sides
-            if facet.aabb.min[plane_axis] < plane_value:
-                left.append(facet)
-            if facet.aabb.max[plane_axis] > plane_value:
-                right.append(facet)
-
-        return left, right
-
-    def candidate_children(
-        self, plane_axis: CoordinateAxes, plane_value: float
-    ) -> Tuple[Tuple[AABB, list[Facet]]]:
-        """Get child AABBs and facet lists from a given splitting plane."""
-        child_boxes = self.aabb.split(plane_axis, plane_value)
-        child_facets = self.split_facets(plane_axis, plane_value)
-
-        return zip(child_boxes, child_facets)
 
     def branch(self, depth: int = 0) -> None:
         """If possible, split the current KDTreeNode into two children nodes."""
@@ -77,6 +40,20 @@ class KDTreeNode:
         # If we've gotten this far, this node is an interrior node
         self.facets = None
 
+    def can_branch(self, depth: int) -> bool:
+        """Return true if this node can be split into two child nodes."""
+        # TODO: Maybe use a more sophisticated cost function to evaluate whether we should branch.
+        return len(self.facets) > 0 and depth < DEPTH_BOUND
+
+    def candidate_children(
+        self, plane_axis: CoordinateAxes, plane_value: float
+    ) -> Tuple[Tuple[AABB, list[Facet]]]:
+        """Get child AABBs and facet lists from a given splitting plane."""
+        child_boxes = self.aabb.split(plane_axis, plane_value)
+        child_facets = self.split_facets(plane_axis, plane_value)
+
+        return zip(child_boxes, child_facets)
+
     def intersect(self, ray: Ray) -> Intersection:
         """Intersect ray with node and return closest found intersection.
 
@@ -85,7 +62,31 @@ class KDTreeNode:
         if not self.aabb.intersect(ray):
             return Intersection.Miss()
 
-        if self.is_leaf():
+        if self.is_leaf:
             return ray.closest_intersection(self.facets)
 
         return ray.closest_intersection(self.children)
+
+    def split_facets(
+        self, plane_axis: CoordinateAxes, plane_value: float
+    ) -> Tuple[list[Facet], list[Facet]]:
+        """Split the node's list of facets into left and right lists based on splitting plane."""
+        left, right = [], []
+
+        for facet in self.facets:
+            # Check minimum bound on left and maximum bound on right.
+            # This covers both scenarios: facets belonging to one side only or both sides
+            if facet.aabb.min[plane_axis] < plane_value:
+                left.append(facet)
+            if facet.aabb.max[plane_axis] > plane_value:
+                right.append(facet)
+
+        return left, right
+
+    def splitting_plane(self, depth: int) -> Tuple[CoordinateAxes, float]:
+        """Return a tuple with the splitting plane axis and value for the given depth."""
+        # TODO: Maybe use a more sophisticated splitting plane generation like SAH.
+        plane_axis = CoordinateAxes(depth % 3)
+        plane_value = self.aabb.center[plane_axis]
+
+        return plane_axis, plane_value
